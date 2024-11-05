9b8031e9d77f874bf185a7946309d83ae5cbb60d
Carine Croteau <carine.croteau@gmail.com>
2024-11-05 18:40:59 -0500

    v0.4

--- src/app/composants/liste-produits/liste-produits.component.ts
+++ src/app/composants/liste-produits/liste-produits.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FicheProduitComponent } from "../fiche-produit/fiche-produit.component";
 import { Produit } from '../../interfaces/produit';
+import { PRODUITS } from '../../mocks/produits';
 
 @Component({
   selector: 'app-liste-produits',
@@ -10,20 +11,5 @@ import { Produit } from '../../interfaces/produit';
   styleUrl: './liste-produits.component.css'
 })
 export class ListeProduitsComponent {
-  produit1: Produit = {
-    image : "https://picsum.photos/id/63/300/200", 
-    nom: "Produit 1", 
-    description: "Description du produit 1",
-    prix: 10, 
-    lien: "#"
-  }
-
-  produit2: Produit = {
-    image : "https://picsum.photos/id/75/300/200", 
-    nom: "Produit 2", 
-    description: "Description du produit 2",
-    prix: 20, 
-    lien: "#"
-  }
-  
+  produits = PRODUITS;
 }
---
+++ src/app/mocks/produits.ts
@@ -0,0 +1,20 @@
+import { Produit } from "../interfaces/produit";
+
+export const PRODUITS:Produit[] = [
+    {
+       // id: 1,
+        image : "https://picsum.photos/id/63/300/200", 
+        nom: "Produit 1", 
+        description: "Description du produit 1",
+        prix: 10, 
+        lien : "#"
+    }, 
+    {
+        //id: 2,
+        image : "https://picsum.photos/id/75/300/200", 
+        nom: "Produit 2", 
+        description: "Description du produit 2",
+        prix: 20, 
+        lien : "#"
+    }
+]
